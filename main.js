58a7a03b8cfdf009541a460b229cd7f472422b0a
Michael <learicist@gmail.com>
2017-12-23 10:43:41 -0500

    update css and javascript

--- main.js
+++ main.js
@@ -1,39 +1,34 @@
-var api = "https://fcc-weather-api.glitch.me/api/current?";
-var lat;
-var lon;
-
 $(document).ready(function(){
-  if (navigator.geolocation) {
-    //If user allows...
-    navigator.geolocation.getCurrentPosition(function(position) {
-      lat = position.coords.latitude;
-      lon = position.coords.longitude;
-      //Get their location
-      $.getJSON((api + "lat=" + lat + "&" + "lon=" + lon), function(json) {
-        var currentCity = json.name;
-        var country = json.sys.country;
-        var temp = Math.floor(json.main.temp);
-        var icon = json.weather[0].icon;
-        $("#gps").html(currentCity + ", " + country);
-        $("#temp").html(temp + "°C");
-        $("#icon").html("<img src=' " + icon + " '/> ");
-        //When user clicks the 'units' button
-        var isCel = true;
-        $("#units").on("click", function(){
-          if (isCel) {
-            $("#temp").html(Math.floor(temp * 1.8 + 32) + "°F");
-            $("#units").html("Switch to Cel");
-            isCel = false;
-          } else {
-            $("#temp").html(temp + "°C");
-            isCel = true;
-            $("#units").html("Switch to Far");
+  
+  $("#random").on("click", function() {
+    $("#random").css("text-decoration", "none");
+  });
+
+  $("#search").on("click", function() {
+    $(".searchBar").html("<form><input id='searchBar' type='text' placeholder='Search Wikipedia'/><button id='searchButton' type='button'><i class='fa fa-search fa-2x' aria-hidden='true'></i></button></form>"); 
+  
+
+    $("#searchButton").on("click", function(){
+      var userInput = $("#searchBar").val();
+      $.getJSON(
+        "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ userInput +"&format=json&callback=?", 
+         function(json) {
+            $("#list").html("");
+            for(var i=0; i<json[1].length; i++) {
+              $("#list").prepend("<li class='links'><a href= " + json[3][i] + ">" + json[1][i] + "</a><p class='descrip'>" + json[2][i] + "</p></li>");
+            }
           }
-        });
-      });
+      );
+      $("#searchBar").val("");
     });
-    //If user rejects prompt
-  } else {
-    $("#gps").html("This browser does not support this service.");
-  }
-});
+    
+    $("#searchBar").keypress(function(e){
+      if(e.which==13) {
+        $("#searchButton").click();
+        e.preventDefault();
+      }
+    });
+  });
+  
+})
+
